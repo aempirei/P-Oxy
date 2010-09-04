b8512ec0e0768ef1bce7271da08ae54f885f5288
Christopher Abad <aempirei@gmail.com>
2010-09-03 18:43:47 -0700

    changed grammar a littl

--- parser.pl
+++ parser.pl
@@ -45,22 +45,90 @@ foreach my $rule (keys(%grammar)) {
 	printf("%s := %s\n", $rule, join(' | ', keys(%{$grammar{$rule}})));
 }
 
+sub grammar_error {
+	my $document = shift;
+	print "grammar error in parsing source code\n";
+}
+
+sub replace_sub {
+
+	my ( $document, $from, $to ) = @_;
+
+	print "next replacement is $from -> $to\n";
+
+	exit;
+}
+exit;
+
+sub get_next_sub {
+
+	my $document = shift;
+
+	my $i = 0;
+
+	while($i < scalar(@$document)) {
+	
+		print "testing offset $i\n";
+
+
+foreach my $rule (keys(%grammar)) {
+	printf("%s := %s\n", $rule, join(' | ', keys(%{$grammar{$rule}})));
+}
+
+
+		foreach my $rule (keys(%grammar)) {
+		}
+
+		# for each offset into the document
+			# for each prefix rule
+				# if prefix rule matches
+					# save prefix rule
+			# if there are matches
+				# return the longest prefix match
+
+		$i += 1;
+	}
+
+	# if we get to this point and no substitution was found then the document contains a grammar error.
+	# just return nothing right now, but eventually the grammar error should be identified as close to
+	# the syntax mistake as possible.
+
+	return;
+}
+
 my @expr;
 
-foreach my $tt (@tokens) {
+my $document = [@tokens];
 
-my ($type, $token) = @$tt;
-
-	# printf("%15s %-15s %s\n", $type, $token, 0);
+while(scalar(@$document) > 1 and $document->[0]->[0] ne 'document') {
 
 	# until primary sequence is <document>
 
-		# while no substitution occurs
-			# consume tokens until complete prefix match is found
-			# if no prefix match is found then output node to queue and step forward in primary
-			# otherwise unget substitution to primary and break
+	my @sub = get_next_sub($document);
 
-		# if queue and primary are same then error
-	
-		# swap queue for primary sequence and rewind
+	if(scalar(@sub) > 0) {
+
+		# if a compressable substitution exists, then perform the replacement
+
+		my ( $from, $to ) = @sub;
+		$document = replace_sub($document, $from, $to);
+
+	} else {
+
+		# if one wasnt found then there is an error in the grammar
+
+		grammar_error();
+		exit;
+	}
 }
+
+# if document is 'document'
+	# then return
+
+# for each possible substitution
+	# attempt substitution
+	# if recurse is 'document'
+		# then return
+
+# return error
+

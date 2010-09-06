603afef6ae56451d134b9b9943ce4c964d6f561c
Christopher Abad <aempirei@gmail.com>
2010-09-06 02:59:55 -0700

    updated this

--- tokenizer.pl
+++ tokenizer.pl
@@ -14,9 +14,9 @@ use P::Lexer;
 
 my $data = join('', <STDIN>);
 
-my @tokens = P::Lexer::get_tokens($data);
+my $tokens = P::Lexer::get_tokens($data);
 
-foreach my $tt (@tokens) {
+foreach my $tt (@$tokens) {
 	my ($type, $token) = @$tt;
 	printf("%15s %-15s\n", $type, $token);
 }
